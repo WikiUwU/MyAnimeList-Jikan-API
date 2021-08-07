659bda8bc3ad95a9b94a8866e867227d779d4e4e
Wiktor Jaworowski <wiktor.jaworowski25@gmail.com>
2021-08-07 15:21:01 +0200

    add content and functionality

---
+++ app.js
@@ -0,0 +1,26 @@
+const ul = document.querySelector("ul");
+
+function createListItem(rank, title, img) {
+    const li = document.createElement("li");
+
+    const html = `
+        <p>${rank}. ${title}</p>
+        <img src="${img}" alt="Image of ${title} anime">
+    `;
+
+    li.innerHTML = html;
+    ul.appendChild(li);
+}
+
+
+
+fetch("https://api.jikan.moe/v3/top/anime", {
+    method: "GET"
+})
+  .then(res => res.json())
+  .then(data => {
+      data.top.forEach(element => {
+          createListItem(element.rank, element.title, element.image_url)
+      });
+  })
+  .catch(err => console.log(err))
